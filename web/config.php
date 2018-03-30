d6ea59123acc93a222ae04fa82836220407f408e
DarknessDark <37892396+DarknessDark@users.noreply.github.com>
2018-03-30 09:54:04 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("AIzaSyDMpSz-G-QrlpN26soA8g7KhjGwYuPTqmE"));
-define('GJ_CODE', getenv("HK"));
-define('SITE_NAME',getenv("MyYouTube"));
-define('TITLENAME', getenv("MyYouTube"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("p1171498212@gmail.com"));
+define('APIKEY', 'AIzaSyDMpSz-G-QrlpN26soA8g7KhjGwYuPTqmE');
+define('GJ_CODE', 'HK');
+define('SITE_NAME', 'MyYouTube');
+define('TITLENAME', 'MyYouTube');
+define('EN2DEKEY', 'EN2DEKEY');
+define('EMAIL', 'p1171498212@gmail.com');
 ?>
